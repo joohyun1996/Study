5f9e5c311442e7b3d702eb76d061f82f5e563940
JooH <nexon9093@gmail.com>
2024-05-31 17:59:13 +0900

    Create String09.java

---
+++ algorithm/boj/문자열/String09.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// boj 5622
+public class String09 {
+    public static void main(String[] args) throws IOException {
+        int[] arr = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10,11,11,11,12,12,12,12};
+        int answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().trim();
+
+        for (char c : str.toCharArray()) {
+            answer += arr[(int) c - 65];
+        }
+        System.out.println(answer);
+    }
+}
