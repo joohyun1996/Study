be5ccb9ef12cfc936fb5de31b49e3da9f548c62a
JooH <nexon9093@gmail.com>
2024-07-17 17:02:28 +0900

    Update 백준_15439.java

--- algorithm/boj/조합론/백준_15439.java
+++ algorithm/boj/조합론/백준_15439.java
@@ -1,1 +1,18 @@
-.
+package combinatorics;
+
+import java.io.*;
+
+// boj 15439
+// 조합이 아니라 단순 곱셈 문제
+public class Comb01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine().trim());
+
+        bw.write(n*(n-1) + "");
+        bw.flush();
+        bw.close();
+    }
+}
