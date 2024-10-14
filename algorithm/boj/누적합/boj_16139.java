528e47125f91daabc8829cd1278ca2d86130a28c
JooH <nexon9093@gmail.com>
2024-10-14 18:09:46 +0900

    Create boj_16139.java

---
+++ algorithm/boj/누적합/boj_16139.java
@@ -0,0 +1,43 @@
+package cumulativeSum;
+
+import java.io.*;
+
+// boj 16139
+
+/**
+ * 문장의 누적된 알파벳을 구하는 문제
+ * 나는 맨 처음에 특정 하나의 문자만 누적합을 구해 정답을 출력하는것을 N 번 반복하려 했으나, 해당 식은 오답임
+ * 따라서 모든 알파벳의 누적합을 계산한 후, 출력만 반복적으로 하면 된다.
+ */
+public class cs3 {
+    static int[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = " " + br.readLine().trim();
+        String[] strs;
+
+        arr = new int[26][str.length()+1];
+
+        int n = Integer.parseInt(br.readLine().trim());
+        int start, end;
+
+        for (int i = 1; i < str.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                arr[j][i] = arr[j][i-1];
+            }
+            arr[str.charAt(i)- 'a'][i] += 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            strs = br.readLine().split(" ");
+            char c = strs[0].charAt(0);
+            start = Integer.parseInt(strs[1])+1;
+            end = Integer.parseInt(strs[2])+1;
+
+            bw.write(arr[c-'a'][end] - arr[c-'a'][start-1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
