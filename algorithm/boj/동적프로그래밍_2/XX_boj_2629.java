c709e1417c1824fcbf499ff5dc7790fc3c133056
JooH <nexon9093@gmail.com>
2025-03-06 20:20:15 +0900

    Update and rename X_boj_2629.java to XX_boj_2629.java

--- algorithm/boj/동적프로그래밍_2/XX_boj_2629.java
+++ algorithm/boj/동적프로그래밍_2/XX_boj_2629.java
@@ -14,6 +14,10 @@ import java.util.Arrays;
  * 2) 방문했으면 기존 값에 더한다 dp[i][j + weight[i-1]] = true;
  * 3) 기존 값에서 빼본다 dp[i][j - weight[i-1]] = true; or dp[i][Math.abs(j - weight[i-1])] = true;
  * 를 반복하고, 최종 구해야 하는 무게는 dp[size][i]에서 t/f를 구한다
+ * 다시 풀었늗네 결국 또 틀렸다. 이 문제에서 점화식을 구할 때 알아야하는게, 
+ * 1) 최대무게를 넘지 않았다면 -> 같은 저울에 두개 놓기, 다른 저울에 각자 놓기 가능
+ * 2) 두 추의 차가 0보다 크다면 -> 한쪽 저울에 있던 추중 하나 빼기
+ * 이 점화식을 잘 생각해야 한다.
  */
 public class dp04 {
     public static void main(String[] args) throws IOException {
